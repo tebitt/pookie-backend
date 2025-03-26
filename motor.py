49cec2f784fc655bb4c233a4279bb825f740c6f6
Jetson <Jetson>
2025-03-26 13:17:52 +0100

    Added more movements (action_3)

--- motor.py
+++ motor.py
@@ -21,14 +21,14 @@ def base():
     move(rm,180) # decrease angle -> rotates up
     move(lm,120) # increase angle -> rotates up
     move(um,180) #decrease angle -> go left
-    move(bm,180)
+    move(bm,90)
 
 def action_1():
     base()
     time.sleep(0.2)
     move(rm,120)
     move(um,120)
-    time.sleep(5)
+    time.sleep(3)
     base()
 
 def action_2():
@@ -43,6 +43,29 @@ def action_2():
         time.sleep(0.2)
     base()
 
+def action_3():
+    base()
+    for i in range(2):
+        move(bm,180)
+        move(lm,180)
+        move(rm,120)
+
+        time.sleep(0.3)
+
+        move(lm,120)
+        move(rm,180)
+
+        time.sleep(0.3)   
+
+        move(bm,60)
+
+
+        time.sleep(0.3) 
+
+    base()
+
+ 
+
 def action_6():
     base()
     for i in range(4):
@@ -54,8 +77,18 @@ def action_6():
         base()
 
         time.sleep(0.3)   
+    
+def test():
+    move(um,180)
 
+action_1()
+time.sleep(1)
 action_2()
+time.sleep(1)
+action_3()
+time.sleep(1)
+action_6()
+time.sleep(1)
 
 # def move(m, target_angle, speed):
 #     current_angle = m.angle
